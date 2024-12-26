cb0e10483a515f2d80dd2f80b94a3c151f8d2ee4
sanju <nsanju013@gmail.com>
2024-12-26 01:52:08 -0500

    Update VariableClass.java

--- StartFromBasics/VariableClass.java
+++ StartFromBasics/VariableClass.java
@@ -1,11 +1,23 @@
 public class VariableClass {
     public static void main(String[] args) {
-        //Declare a variable
+        // Declare variables
         int x = 10;
-        double y  =20;
+        float f = 10.5f;
+        byte b = 127;
+        long l = 1020202l;
+        short s = 500;
+        double y = 20;
+        char c = 's';
+        boolean bo = true;
 
-        System.out.println(x+y);
-
-
+        // Printing
+        System.out.println("Integer: " + x);
+        System.out.println("Float: " + f);
+        System.out.println("Byte: " + b);
+        System.out.println("Long: " + l);
+        System.out.println("Short: " + s);
+        System.out.println("Double: " + y);
+        System.out.println("Character: " + c);
+        System.out.println("Boolean: " + bo);
     }
 }
