c1362ccd94721ac293505077f9952f837d67d749
sanju <nsanju013@gmail.com>
2024-12-26 02:12:40 -0500

    Create TypeConv.java

---
+++ StartFromBasics/TypeConv.java
@@ -0,0 +1,28 @@
+public class TypeConv {
+    public static void main(String[] args) {
+    
+        // byte b=127;
+        // int a=b;
+
+
+        //TYPE CONVERSIONS
+        int a=258;
+        byte b=(byte)a;
+
+        float f=12.3f;
+        int d=(int)f;
+
+        byte x=10;
+        byte y=20;
+        int result=x*y;
+        
+        System.out.println(result);
+
+        System.out.println(d);
+        System.out.println(b);
+    
+
+    
+    }
+
+}
