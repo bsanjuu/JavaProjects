fc397490424a489efeeeb39a1730dcd54594c24e
sanju <nsanju013@gmail.com>
2024-12-26 02:56:46 -0500

    Create OperatorsEg.java

---
+++ StartFromBasics/OperatorsEg.java
@@ -0,0 +1,17 @@
+class OperatorsEg
+{
+    public static void main(String[] args)
+    {
+        //Operators in java
+        int a = 10;
+        int b = 2;
+
+        int add=a+b;
+        int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
+        int mod=a%b;
+        int pow=(int)Math.pow(a,b);
+        System.out.println("add :"+add+" sub :"+sub+" mul :"+mul+" div :"+div+" mod :"+mod+" pow :"+pow);
+    }
+}
