f76ef783d2c410be6e91ce752d0461bad242d567
sanju <nsanju013@gmail.com>
2024-12-26 01:35:50 -0500

    Variables

---
+++ StartFromBasics/VariableClass.java
@@ -0,0 +1,11 @@
+public class VariableClass {
+    public static void main(String[] args) {
+        //Declare a variable
+        int x = 10;
+        double y  =20;
+
+        System.out.println(x+y);
+
+
+    }
+}
