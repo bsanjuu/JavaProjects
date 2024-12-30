56d82bbf9a7fc80da16a3d2d0edb2bc44570ae98
sanju <nsanju013@gmail.com>
2024-12-29 23:28:24 -0500

    Create Arrays.java

---
+++ StartFromBasics/Arrays.java
@@ -0,0 +1,17 @@
+public class Arrays {
+        public static void main(String[] args) {
+            
+            int nums[]=new int[4];
+            nums[0]=4;
+            nums[1]=8;
+            nums[2]=3;
+            nums[3]=9;
+                        
+            for (int i=0;i<4;i++) {
+                System.out.println(nums[i]);
+            }
+            
+        }
+    }
+    
+
