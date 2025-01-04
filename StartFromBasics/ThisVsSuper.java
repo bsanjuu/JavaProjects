3e0aaa63db3e1a0a8a290fd9be86782679428efb
sanju <nsanju013@gmail.com>
2025-01-04 12:18:42 -0500

    Create ThisVsSuper.java

---
+++ StartFromBasics/ThisVsSuper.java
@@ -0,0 +1,20 @@
+class A{
+    int number=10;
+}
+class B extends A{
+    int number=20;
+    public int getValue()
+    {
+        int number=30;
+        return this.number;
+        // return  super.number;
+    }
+}
+
+public class ThisVsSuper {
+    public static void main(String[] args) {
+        B b=new B();
+        System.out.println(b.getValue());
+    }
+
+}
