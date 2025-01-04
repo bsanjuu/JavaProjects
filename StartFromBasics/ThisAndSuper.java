b99049ce2531f34c3e331cf839d5ff89ed807e53
sanju <nsanju013@gmail.com>
2025-01-04 12:15:39 -0500

    Update ThisAndSuper.java

--- StartFromBasics/ThisAndSuper.java
+++ StartFromBasics/ThisAndSuper.java
@@ -2,10 +2,12 @@ class A{
 
     public A()
     {
+        super();
         System.out.println("in A");
     }
     public A(int n)
     {
+        super();
         System.out.println("in A with int");
     }
 }
@@ -14,10 +16,12 @@ class B extends A
 {
     public B()
     {
+        super();
         System.out.println("in B");
     }
     public B(int n)
     {
+		this();  //call constructor of same class
         System.out.println("in B with int");
     }
 
@@ -25,7 +29,9 @@ class B extends A
 
 public class ThisAndSuper {
     public static void main(String[] args) {
-        B b = new B();
+       // B b = new B();
+        B obj=new B(5);
+
 
     }
 }
