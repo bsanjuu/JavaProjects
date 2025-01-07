beac57c88dad50697ec9f0c7b30e873d5a2a870d
sanju <nsanju013@gmail.com>
2025-01-07 02:09:14 -0500

    Packages

---
+++ StartFromBasics/PackageExample.java
@@ -0,0 +1,18 @@
+
+
+public class PackageExample{
+	public static void main(String args[])
+	{
+
+		
+		Calc obj=new Calc();
+		AdvCalc obj1=new AdvCalc();
+		
+		A obj2=new A();
+        
+		
+		System.out.println(obj.add(20, 30));
+	}
+}
+
+
