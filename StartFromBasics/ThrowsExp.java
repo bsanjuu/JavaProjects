56e6bf9b4561f9ff93b67e281d82d17423f6e0c6
sanju <nsanju013@gmail.com>
2025-01-13 01:26:36 -0500

    Create ThrowsExp.java

---
+++ StartFromBasics/ThrowsExp.java
@@ -0,0 +1,22 @@
+class  Sanju
+{
+    void display() throws ClassNotFoundException
+    {
+        Class.forName("Hellooo");
+    }
+
+}
+public class ThrowsExp {
+    public static void main(String[] args) {
+
+        Sanju sanju=new Sanju();
+        try {
+            sanju.display();
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex);
+            ex.printStackTrace();
+        }
+        
+    }
+    
+}
