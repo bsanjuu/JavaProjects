6461d64175f463dea36a177171e28ce504c08e32
sanju <nsanju013@gmail.com>
2025-01-13 02:42:03 -0500

    Create Threads.java

---
+++ StartFromBasics/Threads.java
@@ -0,0 +1,33 @@
+class A extends Thread{
+    public void run(){
+     
+        for (int i = 0; i < 10; i++) {
+            System.out.println("AAA");
+        }
+        
+    }
+}
+
+class  B extends Thread
+{
+    public void run(){
+     
+        for (int i = 0; i < 10; i++) {
+            System.out.println("BBB");
+        }
+        
+    }
+}
+
+public class Threads {
+    
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+
+        a.start();
+        b.start();
+        
+    }
+    
+}
