9d87e9a388155a52912c54378c3447bde0ae4bad
sanju <nsanju013@gmail.com>
2025-01-14 01:20:18 -0500

    Update RunnableNThrowable.java

--- StartFromBasics/RunnableNThrowable.java
+++ StartFromBasics/RunnableNThrowable.java
@@ -1,34 +1,34 @@
-class A implements Runnable
-{
-	public void run()
-	{
-		for(int i=1;i<=5;i++)
-		{
-			System.out.println("Hi");
-			try {
-				Thread.sleep(10);
-			}catch(InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-}
+// class A implements Runnable
+// {
+// 	public void run()
+// 	{
+// 		for(int i=1;i<=5;i++)
+// 		{
+// 			System.out.println("Hi");
+// 			try {
+// 				Thread.sleep(10);
+// 			}catch(InterruptedException e) {
+// 				e.printStackTrace();
+// 			}
+// 		}
+// 	}
+// }
 
-class B implements Runnable
-{
-	public void run()
-	{
-		for(int i=1;i<=5;i++)
-		{
-			System.out.println("Hello");
-			try {
-				Thread.sleep(10);
-			}catch(InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-}
+// class B implements Runnable
+// {
+// 	public void run()
+// 	{
+// 		for(int i=1;i<=5;i++)
+// 		{
+// 			System.out.println("Hello");
+// 			try {
+// 				Thread.sleep(10);
+// 			}catch(InterruptedException e) {
+// 				e.printStackTrace();
+// 			}
+// 		}
+// 	}
+// }
 
 
 
@@ -37,31 +37,31 @@ public class RunnableNThrowable {
     public static void main(String[] args) throws NumberFormatException {   
     	
  	
-        A a = new A();
-        B b = new B();
-        Runnable obj1 = new Thread(a);
-        Runnable obj2 = new Thread(b);
+        // A a = new A();
+        // B b = new B();
+        // Runnable obj1 = new Thread(a);
+        // Runnable obj2 = new Thread(b);
         
 
 
 
-    	// Runnable obj1=()->
-    	// {
-    	// 	for(int i=1;i<=5;i++)
-    	// 	{
-    	// 		System.out.println("Hi");
-    	// 		try {Thread.sleep(10);}catch(InterruptedException e) {e.printStackTrace();}
-    	// 	}
-    	// };
+    	Runnable obj1=()->
+    	{
+    		for(int i=1;i<=5;i++)
+    		{
+    			System.out.println("Hi");
+    			try {Thread.sleep(10);}catch(InterruptedException e) {e.printStackTrace();}
+    		}
+    	};
     	
-    	// Runnable obj2=()->
-    	// {
-    	// 	for(int i=1;i<=5;i++)
-    	// 	{
-    	// 		System.out.println("Hello");
-    	// 		try {Thread.sleep(10);}catch(InterruptedException e) {e.printStackTrace();}
-    	// 	}
-    	// };
+    	Runnable obj2=()->
+    	{
+    		for(int i=1;i<=5;i++)
+    		{
+    			System.out.println("Hello");
+    			try {Thread.sleep(10);}catch(InterruptedException e) {e.printStackTrace();}
+    		}
+    	};
     	
     	 Thread t1=new Thread(obj1);
     	 Thread t2=new Thread(obj2);
