54d51102564e81ef72fefabdea1a71ba0a050592
sanju <nsanju013@gmail.com>
2025-01-15 01:09:04 -0500

    Sets

--- StartFromBasics/CollectionEg.java
+++ StartFromBasics/CollectionEg.java
@@ -1,21 +1,31 @@
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CollectionEg {
     public static void main(String[] args) {
         
         //Collections
-        List<String> list = new ArrayList<String>();
-        list.add("Elderberry");
-        list.add("Apple");
-        list.add("Banana");
-        list.add("Cherry");
-        list.add("Date");
+        // List<String> list = new ArrayList<String>();
+        // list.add("Elderberry");
+        // list.add("Apple");
+        // list.add("Banana");
+        // list.add("Cherry");
+        // list.add("Date");
         
-        list.sort(Comparator.naturalOrder());
+        // list.sort(Comparator.naturalOrder());
 
-        for(String s:list)
+        Set<Integer> set = new HashSet<Integer>();
+        set.add(10);
+        set.add(20);
+        set.add(30);
+        set.add(40);
+        set.add(50);
+        set.add(10);
+
+        
+
+        for(int s:set)
         {
             System.out.println(s);
         }
