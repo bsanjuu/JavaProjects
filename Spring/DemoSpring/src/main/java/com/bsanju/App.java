7b2aed1d9b7874b00183b56af164f5ee9ce57f16
sanju <nsanju013@gmail.com>
2025-01-20 02:28:34 -0500

    DemoSpring Project

---
+++ Spring/DemoSpring/src/main/java/com/bsanju/App.java
@@ -0,0 +1,14 @@
+package com.bsanju;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Dev dev = new Dev();
+       dev.build();
+    }
+}
---
+++ Spring/DemoSpring/src/main/java/com/bsanju/Dev.java
@@ -0,0 +1,14 @@
+package com.bsanju;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+public class Dev {
+
+
+    public void build()
+    {
+        System.out.println("Demo Spring Project");
+    }
+}
